6d1b62b2cf8164b8907a8034250fe3075b896ffb
Stephen Uweh <stephen.uweh@gmail.com>
2021-03-06 11:54:01 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+use Illuminate\Support\Facades\Route;
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+Route::group(['prefix' => 'api/v1'], function () {
+
+    Route::get('/', function(){
+        return "API";
+    });
 });
